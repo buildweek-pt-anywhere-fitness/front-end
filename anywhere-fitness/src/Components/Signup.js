409f981357b06ae6f933aa6d7726a38e3ee9966f
gconnell29 <gconnelljr29@gmail.com>
2021-02-25 00:55:07 -0500

    Started basic form structure for signup component. Currenty has 3 text inputs

--- anywhere-fitness/src/Components/Signup.js
+++ anywhere-fitness/src/Components/Signup.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import '../signup.css'
 
 function Signup() {
   return (
     <div>
-      The sign up form
+      <h3>Please fill out the sign up form</h3>
+      <form className="signup-form">
+        <label> First Name
+          <input
+            name="fname"
+            type="text"
+          />
+        </label>
+        <label> Last Name
+          <input
+            name="lname"
+            type="text"
+          />
+        </label>
+        <label className='email-label'> Email
+          <input
+            name="email"
+            type="email"
+          />
+        </label>
+        {/* training style */}
+        {/* fitness level */}
+      </form>
     </div>
   );
 }
