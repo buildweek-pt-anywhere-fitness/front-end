7b42551d541fc35e7b795d550930d29881cd4399
gconnell29 <gconnelljr29@gmail.com>
2021-03-04 21:29:39 -0500

    create Home component and added Routes to toggle between Home and Signup components

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 import Signup from './Components/Signup.js';
+import Home from './Components/Home.js';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Signup />
+      <Link to='/'><button>Home</button></Link>
+      <Link to='/signup'><button>Register</button></Link>
+      <Route exact path='/'><Home /></Route>
+      <Route path='/signup'><Signup /></Route>
     </div>
   );
 }
