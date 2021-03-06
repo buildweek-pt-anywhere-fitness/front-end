e58a138ee2047282a94912aff3979aafedddf5d6
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-03-05 22:41:23 -0700

    fix conflicts attempt.

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import Login from './components/Login';
 import Signup from './components/Signup.js';
 import Instructor from './components/Instructor';
 import CreateClass from './components/CreateClass';
 import UpdateClass from './components/UpdateClass';
-import logo from './logo.svg';
 import './App.css';
 import PrivateRoute from "./Components/PrivateRoute";
 
 function App() {
   return (
     <div className="App">
-      App is online.
       <Router>
         <Route exact path='/instructorclasses' component={Instructor} />
         <Route path='/instructorclasses/:id' component={UpdateClass} />
--- anywhere-fitness/src/Components/Instructor.js
+++ anywhere-fitness/src/Components/Instructor.js
@@ -18,20 +18,21 @@ function Instructor() {
     
     return (
         <div>
-            <Link to='/newclass'>Add class</Link>
-                {classes.map(c => (
-                    <div key={c.id} className='workout'>
-                        <h3>{c.name}</h3>
-                        <p>{c.type}</p>
-                        <p>{c.intensity_level}</p>
-                        <p>Begins: {c.start_time}</p>
-                        <p>Lasts: {c.duration}</p>
-                        <p>Location: {c.location}</p>
-                        <p>Registered: {c.registered}</p>
-                        <p>Max class size: {c.max_class_size}</p>
-                        <Link to={`/instructorclasses/${c.id}`}>edit</Link>
-                    </div>
-                ))}
+             <Link to='/newclass'>Add class</Link>
+            {classes.map(c => (
+                <div key={c.id}>
+                    <h3>{c.name}</h3>
+                    <p>{c.type}</p>
+                    <p>{c.intensity_level}</p>
+                    <p>Begins: {c.start_time}</p>
+                    <p>Lasts: {c.duration}</p>
+                    <p>Location: {c.location}</p>
+                    <p>Registered: {c.registered}</p>
+                    <p>Max class size: {c.max_class_size}</p>
+                    <Link to={`/instructorclasses/${c.id}`}>edit</Link>
+                    <button onClick={() => deleteClass(c.id)}>Delete</button>
+                </div>
+            ))}
         </div>
     );
 };
