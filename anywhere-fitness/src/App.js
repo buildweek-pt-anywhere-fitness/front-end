b825e36f83802b2cc6b6a28d6451b1a0c2ea14d6
gconnell29 <gconnelljr29@gmail.com>
2021-03-06 01:13:26 -0500

    create Status component place holder

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 import Signup from './Components/Signup.js';
+import Status from './Components/Status.js';
 import Home from './Components/Home.js';
 import logo from './logo.svg';
 import './App.css';
@@ -10,8 +11,10 @@ function App() {
     <div className="App">
       <Link to='/'><button>Home</button></Link>
       <Link to='/signup'><button>Register</button></Link>
+      <Link to='/status'><button>Status</button></Link>
       <Route exact path='/'><Home /></Route>
       <Route path='/signup'><Signup /></Route>
+      <Route path='/status'><Status /></Route>
     </div>
   );
 }
---
+++ anywhere-fitness/src/Components/Status.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Status(props) {
+  
+  return (
+    <div>
+      Your status will be shown here.
+    </div>
+  );
+}
+
+export default Status;
