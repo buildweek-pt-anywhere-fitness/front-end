bd0dc9016fec643e8d65bb21239f0ee452449e76
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-03-05 22:28:12 -0700

    fix conflicts attempt 3

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 import Login from './components/Login';
-import Signup from './components/Signup';
+import Signup from './components/Signup.js';
 import Instructor from './components/Instructor';
 import CreateClass from './components/CreateClass';
 import UpdateClass from './components/UpdateClass';
