8bfd46d7665874b212fa3f07ef1bb924a3606732
gconnell29 <gconnelljr29@gmail.com>
2021-03-07 00:14:26 -0500

    refactored/shotertend Signup component.

--- anywhere-fitness/src/Components/Signup.js
+++ anywhere-fitness/src/Components/Signup.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
 import {BrowserRouter, Link, Route} from 'react-router-dom';
-import Checkbox from './Checkbox';
+// import Checkbox from './Checkbox';
+// import Billing from './Billing';
 import '../signup.css'
 
 function Signup() {
+  const trainingOptions = ['Running', 'Calisthenics', 'Power Lifting', 'Boxing', 'Cycling', 'Weight Lifting', 'Yoga', 'Dancing'];
+  const billingOptions = ['Street', 'City', 'State', 'Zip Code', 'Cardholder Name', 'Card Number', 'Security Code'];
+  const paymentOptions = ['Visa', 'Venmo', 'Paypal'];
+  const personal = ['First Name', 'Last Name', 'Email', 'Town', 'zip'];
+  
   const [client, setClient] = useState({
-    fname: '',
-    lname: '',
+    first_name: '',
+    last_name: '',
     email: '',
     town: '',
-    locationZip: '',
+    zip: '',
     exp: '',
-    // checked: false,
     training: [],
     payment: '',
     street: '',
     city: '',
     state: '',
-    paymentZip: '',
-    cardName: '',
-    cardNumber: '',
-    code: ''
+    zip_code: '',
+    cardholder_name: '',
+    card_number: '',
+    security_code: ''
   });
-
-  const trainingOptions = ['Running', 'Calisthenics', 'Power Lifting', 'Boxing', 'Cycling', 'Weight Lifting', 'Yoga', 'Dancing'];
 
   function handleChange(event) {
     let value = event.target.value;
@@ -49,138 +52,81 @@ function Signup() {
   return (
     <div>
       <h3>Please fill out the sign up form</h3>
-      <form className="signup-form">
-        <label> First Name
-          <input
-            name="fname"
-            type="text"
-            value={client.fname}
-            onChange={handleChange}
-          />
-        </label>
-        <label> Last Name
-          <input
-            name="lname"
-            type="text"
-            value={client.lname}
-            onChange={handleChange}
-          />
-        </label>
-        <label className='email-label'> Email
-          <input
-            name="email"
-            type="email"
-            value={client.email}
-          />
-        </label>
-        <label>Town
-          <input
-            name="town"
-            type="text"
-            value={client.town}
-          />
-        </label>
-        <label> Zip Code
-          <input
-            name="locationZip"
-            type="number"
-            value={client.locationZip}
-          />
-        </label>
+      <form className='signup-form'>
+        {personal.map(option => {
+          return (
+            <label> {option}
+              <input
+              name={option.toLowerCase().replace(' ', '_')}
+              type='text'
+              value={client.option}
+              onChange={handleChange}
+              />
+            </label>
+          )
+        })}
+        
         <h3>Experience Level</h3>
-        <label className="exp-label"> Beginner
+        <label className='exp-label'> Beginner
           <input
-            name="exp"
-            type="radio"
-            value="beginner"
+            name='exp'
+            type='radio'
+            value='beginner'
             checked={client.exp === 'beginner'}
           />
         </label>
-        <label className="exp-label"> Intermediate
+        <label className='exp-label'> Intermediate
           <input
-            name="exp"
-            type="radio"
-            value="internmediate"
+            name='exp'
+            type='radio'
+            value='internmediate'
             checked={client.exp === 'internmediate'}
           />
         </label>
-        <label className="exp-label"> Advanced
+        <label className='exp-label'> Advanced
           <input
-            name="exp"
-            type="radio"
-            value="advanced"
+            name='exp'
+            type='radio'
+            value='advanced'
             checked={client.exp === 'advanced'}
           />
         </label>
         <h3>Preferred Training Style</h3>
         {trainingOptions.map(option => {
-          return <Checkbox name={option} change={handleCheckbox} />;
+          return (
+          <label>
+            <input className='training-input'
+              name='training'
+              type='checkbox'
+              onChange={handleCheckbox}
+              value={option}
+            /> {option}
+          </label>)
+        })}
+
+        <h3>Payment Information</h3>
+        {paymentOptions.map(option => {
+          return (
+          <label className='pay-label'> {option}
+            <input 
+              name='payment'
+              type='radio'
+              onChange={handleChange}
+            />
+          </label>)
         })}
         
-        <h3>Payment Information</h3>
-        <label className="pay-label"> Visa
-          <input
-            name="payment"
-            type="radio"
-          />
-        </label>
-        <label className="pay-label"> Venmo
-          <input
-            name="payment"
-            type="radio"
-          />
-        </label>
-        <label className="pay-label"> Paypal
-          <input
-            name="payment"
-            type="radio"
-          />
-        </label>
         <h4>Billing Address</h4>
-        <label> Street
-          <input
-            name="street"
-            type="text"
+        {billingOptions.map(option => {
+          return <label> {option}
+          <input className={option.toLowerCase().replace(' ', '_')}
+            name={option.toLowerCase().replace(' ', '_')}
+            type='text'
+            onChange={handleChange}
           />
         </label>
-        <label> City
-          <input
-            name="city"
-            type="text"
-          />
-        </label>
-        <label> State
-          <input
-            name="state"
-            type="text"
-          />
-        </label>
-        <label> Zip Code
-          <input
-            name="paymentZip"
-            type="text"
-          />
-        </label>
-        <h4>Card Info</h4>
-        <label> Cardholder Name
-          <input
-            name="cardName"
-            type="text"
-          />
-        </label>
-        <label> Card Number
-          <input
-            name="cardNumber"
-            type="number"
-          />
-        </label>
-        <label> Security Code
-          <input className="code-input"
-            name="code"
-            type="number"
-          />
-        </label>
-        <button className="submit-btn" onClick={handleSubmit}>Submit</button>
+        })}
+        <button className='submit-btn' onClick={handleSubmit}>Submit</button>
       </form>
     </div>
   );
